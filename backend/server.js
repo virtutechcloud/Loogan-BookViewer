041d73476ace2e87228aac63dd2e04e51940db9a
FrostHash <virtutechcloudinnovations@gmail.com>
2025-06-14 00:36:42 -0400

    made changes to server port to 5001

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
